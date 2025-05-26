03d10f070067ee90fd86cb938d6027c84d603576
Gabriel Machado <127336276+P3RALT@users.noreply.github.com>
2025-05-26 14:52:42 -0300

    Add files via upload

---
+++ Lista04_Atv06.java
@@ -0,0 +1,15 @@
+package lista04;
+public class Lista04_Atv06 {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+        
+        System.out.println("-----------");
+        
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " "); // todos na mesma linha
+        }
+      
+    } }
